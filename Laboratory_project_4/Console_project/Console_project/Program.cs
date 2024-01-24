180111eff18df16551e1d060398d89bc8d5b6d15
AndreyRazin <110714747+AndreyRazin007@users.noreply.github.com>
2024-01-24 08:54:55 +0500

    Add files via upload

---
+++ Laboratory_project_4/Console_project/Console_project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console_project
+{
+    class Program
+    {
+        private static void Main()
+        {
+            double number_1 = Convert.ToDouble(Console.ReadLine());
+            double number_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Math.Pow(number_1, number_2));
+        }
+    }
+}
